ba933bd4c433b72c6104c5faefb62f1af81a50f9
Chimmie Firefly <gameplayer2019pl@tutamail.com>
2025-04-17 19:30:10 +0100

    Link @art@mrrp.chimmie.k.vu as official account.

--- index.sh
+++ index.sh
@@ -12,6 +12,7 @@ echo '<body>' >> index.html
 cat res/source.html >> index.html
 echo '<a hidden="true" rel="me" href="https://catcatnya.com/@gameplayervent">Mastodon</a>' >> index.html
 echo '<a hidden="true" rel="me" href="https://mrrp.chimmie.k.vu/@gameplayer">Mrrp instance</a>' >> index.html
+echo '<a hidden="true" rel="me" href="https://mrrp.chimmie.k.vu/@art">Mrrp instance - Art Account</a>' >> index.html
 echo '<div class="maindiv">' >> index.html
 echo '<div class="main">' >> index.html
 markdown INDEX.md >> index.html
